9b2d09717db90ef8e58befce2a745923305168d3
Bartira Máximo Arpini <bartmaximo1.0@gmail.com>
2026-01-21 17:01:59 -0300

    fim da aula 5

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -13,19 +13,21 @@ class Gafanhoto:
     # Métodos de Instância
     def aniversario(self):
         self.idade += 1
-
-    def mensagem(self):
+    
+    def __str__(self):  # mostrar os dados de uma forma mais "amigável"
+        #  dounder method
         return f'{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade.'
+    
+    def __getstate__(self):
+        return f'Estado: nome = {self.nome} ; idade = {self.idade}'
     
 # Declaração de Objetos
 g1 = Gafanhoto('Maria', 17)  
 g1.aniversario()
-print(g1.mensagem())
+print(g1)
 
-g2 = Gafanhoto('Mauro', 23)
-print(g2.mensagem())
+print(g1.__doc__)  # dunder attribute
 
-g3 = Gafanhoto()
-print(g3.mensagem())
-
-print(g1.__doc__)
+print(g1.__dict__) # atributo
+print(g1.__getstate__()) # método editável
+print(g1.__class__)
---
+++ exercicios/ex003.py
@@ -0,0 +1,31 @@
+class ContaBancaria:
+    '''
+    Docstring for ContaBancaria:
+    Cria uma conta bancária e permite fazer saques e depósitos 
+    '''
+    def __init__(self, id, nome, saldo = 0):
+        self.id = id
+        self.titular = nome
+        self.saldo = saldo
+        print(f'Conta {self.id} criada com sucesso! Saldo atual de R$ {self.saldo:,.2f}')
+
+    def __str__(self):
+        return f'A conta n° {self.id} de {self.titular} possui R$ {self.saldo:,.2f} de saldo'
+
+    def deposito(self, valor):
+        self.saldo += valor
+        print(f'Depósito de R$ {valor:,.2f} autorizado na conta {self.id}')
+
+    def saque(self, valor):
+        if valor > self.saldo:
+            print(f'Saque NEGADO de R$ {valor:,.2f} na conta {self.id}: SALDO INSUFUCIENTE')
+        else:
+            self.saldo -= valor
+            print(f'Saque de R$ {valor:,.2f} autorizado na conta {self.id}')
+        
+    
+
+c1 = ContaBancaria(112, 'Gustavo', 3000)
+c1.deposito(500)
+c1.saque(20000)
+print(c1)
