9e53714ab4da1d114eaba9dede05f86bf49931bd
Bartira Máximo Arpini <bartmaximo1.0@gmail.com>
2026-02-06 18:33:19 -0300

    amem

--- desafios/desafio_0022.py
+++ desafios/desafio_0022.py
@@ -11,18 +11,25 @@ print(Panel(texto, title=f'[ TV ]', title_align='center', width=40))
 print(Panel(f'CANAL = 1 2 3 4 5\nVOLUME = ▮▮▮▯', title=f'[ TV ]', title_align='center', width=40))
 
 
+volume_nivel = 1
+MAX_VOLUME = 5
 
 
-tela_volume = '▮▯▯▯'
+tela_volume = '▮' * volume_nivel + '▯' * (MAX_VOLUME - volume_nivel)
+print(f'Volume: {tela_volume}')
 
 while True:
-    volume = input('Volume: ') 
-    if volume == '+':
-        tela_volume[0] = '▮'
-        tela_volume[:-1]
-    elif volume == '0':
-        break   
-print(tela_volume)
+    volume = input('Volume: ')
+    if volume_nivel < MAX_VOLUME:
+        if volume == '+':
+            tela_volume = '▮' + tela_volume
+            tela_volume = tela_volume[:-1]
+        elif volume == '-':
+            tela_volume = tela_volume + '▯'
+            tela_volume = tela_volume[1:]
+        elif volume == '0':
+            break   
+    print(tela_volume)
 
 
 
